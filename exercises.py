b5482fdd9c4144ede87f27c615e883e11e9826a8
Mr Malaitai (School) <49211523+Malaitai@users.noreply.github.com>
2021-08-09 09:25:19 +1200

    Update and rename exercise_1.py to exercises.py

---
+++ exercises.py
@@ -0,0 +1,42 @@
+# Name: 
+# Date: 
+# Teacher:
+
+# Exercise One
+# Print a string that displays "Kia ora, World!"
+
+
+# Exercise Two
+# Create a loop that prints 1 to 10
+
+
+# Exercise Three
+# Create a list and print each item in the list
+
+
+# Exercise Four
+# Print each item, using the same list, in descending order
+
+
+# Exercise Five
+# Ask the user for their name using input and print a greeting with their name
+
+
+# Exercise Six
+# Ask the user for a number between 1 and 10 and print the number plus 5
+
+
+# Exercise Seven
+# Ask the user how they are feeling. If they reply "happy", print "Good to hear". If they reply "sad"", print "Sorry to hear that". If they reply with anything else, print "Intersting."
+
+
+# Exercise Eight
+# Ask the user for their name, birthday and favourite colour, separate each into their own items in a list. Print the list.
+
+
+# Exercise Nine
+# From the previous exerise, put all the list into a .txt file named "information.txt"
+
+
+# Exercise Ten
+# Read from the "information.txt" file and print what is inside the .txt file
